072257ba4ba2cf81f70d3a0852d9b57307fe74fe
Miguel Bonilla <bonilla7710@gmail.com>
2019-05-09 17:24:52 -0400

    modified casing scheme, able to pull nbma interface

--- bgpInfo.py
+++ bgpInfo.py
@@ -12,49 +12,49 @@ import logging
 
 class VerifyUserInput(object):
     """
-    Verify User input: ciName against the /etc/host file.
-    return ciname + domain name:
+    Verify User input: ci_name against the /etc/host file.
+    return ci_name + domain name:
     wp-nwk-atm-xr.gpi.remote.binc.net
     """
-    def __init__(self, ciName=None):
-        self.ciName = ciName
+    def __init__(self, ci_name=None):
+        self.ci_name = ci_name
         self.verified = None
         
-    def verifyEtcHost(self):
-        bgpLogger.info('inside VerifyEtcHost() method')
+    def verify_etc_hosts(self):
+        bgp_logger.info('inside verify_etc_hosts() method')
         ''' run cat /etc/hosts and get list of devices '''
         # declaring function scope variable
-        if self.ciName == None:
-            print("You didn't include ciName")
+        if self.ci_name == None:
+            print("You didn't include ci_name")
             return False
         else:
-            hostPattern = re.compile(r'\s+(%s)'%self.ciName, re.IGNORECASE)
+            host_pattern = re.compile(r'\s+(%s)'%self.ci_name, re.IGNORECASE)
             try:
                 proc = subprocess.Popen(
                         ['cat','/etc/hosts'], stdout=subprocess.PIPE)
                 stdout = proc.communicate()[0]
                 stdout = stdout.split('\n')
             except Exception as err:
-                bgpLogger.info(err)
+                bgp_logger.info(err)
                 raise SystemExit(
                     "I am not able to find your BGP ROUTER on this BMN\n")
-        # Initialize the verified variable if ciName is not found in
+        # Initialize the verified variable if ci_name is not found in
         # /etc/hosts script will exit
         for line in stdout:
-            if hostPattern.search(line):
+            if host_pattern.search(line):
                 verified = True
                 if len(line.split()) == 3:
-                    bgpLogger.info(line.split()[1])
-                    ciFQDN = line.split()[1]
-                    return ciFQDN
+                    bgp_logger.info(line.split()[1])
+                    ci_fqdn = line.split()[1]
+                    return ci_fqdn
                 else:
-                    bgpLogger.info("This looks different\n" + line)
+                    bgp_logger.info("This looks different\n" + line)
                     return False
-                bgpLogger.info(hostPattern.search(line).group(0).strip())
+                bgp_logger.info(host_pattern.search(line).group(0).strip())
         # verified will be None if no FQDN was found
         if self.verified == None:
             print("I cannot find %s as a managed device"
-                         " in this BMN"%self.ciName)
+                         " in this BMN"%self.ci_name)
             return False
 
 
@@ -64,21 +64,21 @@ class LoggerClass(object):
     that can store logging info in a file. This file is located
     in the same directory where the script is running by default.
     To have the script generate script logging remove the hash in the 
-    commented out lines below."""
+    commented out lines below. """
     @staticmethod
     def logging():
         today = datetime.date.today()
         mydate = (str(today.year) + "-" + str(today.month) + 
                  "-" + str(today.day))
 
-        # LOG_FILENAME = "bgpInfoScript_" + mydate + ".log"
-        global bgpLogger
-        bgpLogger = logging.getLogger(__name__)
-        bgpLogger.setLevel(logging.INFO)
-        bgpLogger.disabled = False
+        # log_filename = "bgpInfoScript_" + mydate + ".log"
+        global bgp_logger
+        bgp_logger = logging.getLogger(__name__)
+        bgp_logger.setLevel(logging.INFO)
+        bgp_logger.disabled = False
 
-        # self.fileLog = logging.FileHandler(LOG_FILENAME)
-        # self.fileLog.setLevel(logging.INFO)
+        # self.file_log = logging.FileHandler(log_filename)
+        # self.file_log.setLevel(logging.INFO)
 
         streamLog = logging.StreamHandler()
         streamLog.setLevel(logging.INFO)
@@ -86,62 +86,115 @@ class LoggerClass(object):
         formatter = logging.Formatter('%(asctime)s - %(levelname)s '
                                       '- %(message)s')
 
-        # self.fileLog.setFormatter(formatter)
+        # self.file_log.setFormatter(formatter)
         streamLog.setFormatter(formatter)
 
-        # self.bgpLogger.addHandler(fileLog)
-        bgpLogger.addHandler(streamLog)
+        # self.bgp_logger.addHandler(file_log)
+        bgp_logger.addHandler(streamLog)
 
 
-class BgpCommands(object):
+class CiscoCommands(object):
     ''' This class will run any bgp related commands '''
 
-    def __init__(self, ciName):
-        self.ciName = ciName
+    def __init__(self, ci_name):
+        self.ci_name = ci_name
         self.command = None
 
-    def verifyIPprotocols(self):
+    def verify_ip_protocols(self):
         ''' This method will verify BGP is configured '''
-        bgpLogger.info('verifyIPprotocols() method')
+        bgp_logger.info('verify_ip_protocols() method')
         self.command = 'show ip protocol | s bgp'
-        bgpPattern = re.compile(r'(bgp\s+\d+)')
-        output = self.runCiscoCommand()
+        bgp_as_pattern = re.compile(r'(bgp\s+\d+)')
+        output = self.run_cisco_commands()
         for line in output:
-            if bgpPattern.search(line):
+            if bgp_as_pattern.search(line):
                 print("This device runs BGP: %s"%
-                      bgpPattern.search(line).group(1))
+                      bgp_as_pattern.search(line).group(1))
                 return True
             
-    def cleanCloginOutput(self,cloginOutput):
-        bgpLogger.info('cleanCloginOutput() method')
+    def clean_clogin_output(self,clogin_output):
+        bgp_logger.info('clean_clogin_output() method')
         ''' remove prompt output from clogin output '''
-        for index, line in enumerate(cloginOutput):
+        for index, line in enumerate(clogin_output):
             if self.command in line:
                 start = index
             if 'exit' in line:
                 end = index
-        return cloginOutput[start:end]
+        return clogin_output[start:end]
 
-    def runCiscoCommand(self):
-        bgpLogger.info('runCiscoCommand() method')
+    def run_cisco_commands(self):
+        bgp_logger.info('run_cisco_commands() method')
         ''' Run clogin to retrieve command information 
         from device '''
         try:
-            cloginProcess = subprocess.Popen(['sudo','-u','binc',
+            clogin_process = subprocess.Popen(['sudo','-u','binc',
                                           '/opt/sbin/clogin',
-                                          '-c',self.command,self.ciName],
+                                          '-c',self.command,self.ci_name],
                                           stdout=subprocess.PIPE)
-            cloginOutput = cloginProcess.communicate()[0]
-            cloginOutput = cloginOutput.split('\r\n')
-            return self.cleanCloginOutput(cloginOutput)
+            clogin_output = clogin_process.communicate()[0]
+            clogin_output = clogin_output.split('\r\n')
+            return self.clean_clogin_output(clogin_output)
         except Exception as err:
             raise SystemExit('clogin process failed for device: %s\n'
-                             'ERROR: %s'%(self.ciName, err))
+                             'ERROR: %s'%(self.ci_name, err))
+
+    def show_ip_cef(self, ip_address):
+        bgp_logger.info('show_ip_cef() method')
+        self.command = 'show ip cef ' + ip_address
+        output = self.run_cisco_commands()
+        cef_interface_pattern = re.compile(r'(?:\S+\s+)(\S+)$')
+        for line in output[1:]:
+            if cef_interface_pattern.search(line):
+                return cef_interface_pattern.search(line).group(1)
+
+    def show_dmvpn_interface(self, source_interface, neighbor_ip):
+        ''' retrieve dmvpn information '''
+        bgp_logger.info('show_dmvpn_interface() method')
+        self.command = ('show dmvpn interface ' + source_interface + 
+                        ' | i ' + neighbor_ip)
+
+        dmvpn_output_pattern = re.compile(r'(?:^\s+\d+\s)(\S+)(?:\s+\S+\s+)')
+        output = self.run_cisco_commands()
+        for line in output[1:]:
+            if dmvpn_output_pattern.search(line):
+                return dmvpn_output_pattern.search(line).group(1)
+
+    def show_vrf_config(self, nbma_interface):
+        ''' this method will verify if nbma address 
+            is reachable through a vrf '''
+        bgp_logger.info('show_vrf_config() method')
+        self.command = ('show ip vrf')
+        interface_number_pattern = re.compile(r'(?:\w+)(\d+\S+)')
+        vrf_name_pattern = re.compile(r'(?:^\s+)(\S+)(\s+)')
+        interface_number = str(
+            interface_number_pattern.match(nbma_interface).group(1))
+        output = self.run_cisco_commands()
+        for line in output[1:]:
+            if interface_number in line:
+                return vrf_name_pattern.search(line).group(1)
+
+    def ping_through_vrf(self, vrf_name, nbma_end_ip):
+        ''' this method will ping nbma end point ip address 
+            through vrf to test carrier connectivity '''
+        bgp_logger.info('ping_through_vrf() method')
+        self.command = "ping vrf " + vrf_name + " " + nbma_end_ip
+        output = self.run_cisco_commands()
+        for line in output[1:]:
+            print(line)
+
+    def ping_through_to_end_ip(self, nbma_end_ip):
+        ''' this method will ping nbma end point ip address to test
+            ip connectivity '''
+        bgp_logger.info('ping_through_to_end_ip() method')
+        self.command = "ping " + nbma_end_ip
+        output = self.run_cisco_commands()
+        for line in output[1:]:
+            print(line)
 
 
-def argumentParser():
+def argument_parser():
     ''' Run argument parser to verify what user wants to do '''
-    parser = OptionParser(usage="\nOPTION: %prog -d <ciName> "
+    parser = OptionParser(usage="\nOPTION: %prog -d <ci_name> "
                                 "-n <ipAddress>\n\n"
     "EXAMPLE: bgpInfo -d wp-nwk-atm-xr.gpi.remote.binc.net -n 8.9.10.11\n\n"
     "ALSO TO PRINT HELP: %prog --help to print this information",
@@ -149,43 +202,62 @@ def argumentParser():
     parser.add_option("-d", "--device",
                     # optional because action defaults to "store"
                     action="store", 
-                    dest="ciName",
-                    help="ciName is a REQUIREMENT to run this script",)
+                    dest="ci_name",
+                    help="ci_name is a REQUIREMENT to run this script",)
     parser.add_option("-n", "--neighbor",
                     action="store",
                     dest="neighbor_ip",
                     help="BGP Neighbor IP address is a REQUIREMENT",)
     (options, args) = parser.parse_args()
-    if options.ciName and options.neighbor_ip:
-        usrInput = VerifyUserInput(options.ciName)
-        ciNameVerified = usrInput.verifyEtcHost()
-        if not ciNameVerified:
+    if options.ci_name and options.neighbor_ip:
+        user_input = VerifyUserInput(options.ci_name)
+        ci_name_verified = user_input.verify_etc_hosts()
+        if not ci_name_verified:
             raise SystemExit('Terminating Script!')
         else:
-            return ciNameVerified
+            return (ci_name_verified, options.neighbor_ip)
     else:
-        parser.error("You need to provide ciName and BGP Neighbor"
+        parser.error("You need to provide ci_name and BGP Neighbor"
                      " IP to run this Script\n\n")
 
 
-def bgpOrchestrator(ciFQDN):
-    bgpLogger.info('bgpOrchestrator() method')
-    BGP = BgpCommands(ciFQDN)
-    if BGP.verifyIPprotocols():
-        print("This router runs: %s"%BGP)
-        
+def bgp_orchestrator(ci_fqdn, neighbor_ip):
+    bgp_logger.info('bgp_orchestrator() method')
+    bgp = CiscoCommands(ci_fqdn)
+    bgp_as = bgp.verify_ip_protocols()
+    if bgp_as:
+        source_interface = bgp.show_ip_cef(neighbor_ip)
+        print(source_interface)
+        if 'Tunnel' in source_interface:
+            nbma_end_ip = bgp.show_dmvpn_interface(source_interface,
+                                                     neighbor_ip)
+            print(nbma_end_ip)
+        if nbma_end_ip:
+            nbma_interface = bgp.show_ip_cef(nbma_end_ip)
+            print(nbma_interface)
+        if nbma_interface:
+            vrf_name = bgp.show_vrf_config(nbma_interface)
+        if vrf_name:
+            print("Testing Connectivity to: " + nbma_end_ip + "through: " + 
+                vrf_name + "\n")
+            ping_vrf_results = bgp.ping_through_vrf(vrf_name, nbma_end_ip)
+        if not vrf_name:
+            print("Testing Connectivity to: " + nbma_end_ip + "\n")
+            ping_results = bgp.ping_through_to_end_ip(nbma_end_ip)
+    else:
+        raise SystemExit("This device does not run BGP")
 
 
 if __name__ == '__main__':
     #  bgpInfo -d wp-nwk-atm-xr.gpi.remote.binc.net
     # Initializing Dictionary to Store BGP information
-    bgpDict = lambda: defaultdict(bgpDict)
-    bgpInfoDict = bgpDict()
-    __slots__ = bgpInfoDict
+    bgp_dict = lambda: defaultdict(bgp_dict)
+    bgp_info_dict = bgp_dict()
+    __slots__ = bgp_info_dict
 
     # Initialize logging module
     LoggerClass.logging()
 
-    ciFQDN = argumentParser()
+    ci_fqdn, neighbor_ip = argument_parser()
 
-    bgpOrchestrator(ciFQDN)
+    bgp_orchestrator(ci_fqdn, neighbor_ip)
--- test_bgpInfo.py
+++ test_bgpInfo.py
@@ -1,40 +1,43 @@
 import unittest
-import bgpInfo
+from bgpInfo import LoggerClass, VerifyUserInput
 
-class TestVerifyUserInput(unittest.TestCase):
+class TestVerifyuser_input(unittest.TestCase):
     """Testing script on gpi-s-bmn03
        It needs BMN to be able to return a valid
        ciName from /etc/hosts file.
     """
 
-    @classmethod
-    def setUpClass(cls):
-        """Initialize VerifyUserInput class"""
+    def setUp(self):
+        """Initialize Verifyuser_input class"""
         LoggerClass.logging()
 
-    def test_ciNameEqualNone(self):
+    def test_device_input_equal_none(self):
+        print("\n")
         print("Starting ciName test if empty")
-        userInput = VerifyUserInput()
-        fromUser = userInput.verifyEtcHost()
-        self.assertEqual(fromUser, False)
+        user_input = VerifyUserInput()
+        from_user = user_input.verify_etc_hosts()
+        self.assertEqual(from_user, False)
 
-    def test_ciNameNotFound(self):
+    def test_device_name_not_found(self):
+        print("\n")
         print("Testing ciName not found in BMN")
-        userInput = VerifyUserInput('wp-nwk-atm01')
-        fromUser = userInput.verifyEtcHost()
-        self.assertEqual(fromUser, False)
+        user_input = VerifyUserInput('wp-nwk-atm01')
+        from_user = user_input.verify_etc_hosts()
+        self.assertEqual(from_user, False)
 
-    def test_ciNameShort(self):
+    def test_device_name_short(self):
+        print("\n")
         print("Testing ciName short name 'wp-nwk-atm-xr'")
-        userInput = VerifyUserInput('wp-nwk-atm-xr')
-        fromUser = userInput.verifyEtcHost()
-        self.assertEqual(fromUser, 'wp-nwk-atm-xr.gpi.remote.binc.net')
+        user_input = VerifyUserInput('wp-nwk-atm-xr')
+        from_user = user_input.verify_etc_hosts()
+        self.assertEqual(from_user, 'wp-nwk-atm-xr.gpi.remote.binc.net')
 
-    def test_ciNameLong(self):
-        print("Testing ciName short name 'wp-nwk-atm-xr.gpi.remote.binc.net'")
-        userInput = VerifyUserInput('wp-nwk-atm-xr.gpi.remote.binc.net')
-        fromUser = userInput.verifyEtcHost()
-        self.assertEqual(fromUser, 'wp-nwk-atm-xr.gpi.remote.binc.net')
+    def test_device_name_log(self):
+        print("\n")
+        print("Testing ciName name FQDN 'wp-nwk-atm-xr.gpi.remote.binc.net'")
+        user_input = VerifyUserInput('wp-nwk-atm-xr.gpi.remote.binc.net')
+        from_user = user_input.verify_etc_hosts()
+        self.assertEqual(from_user, 'wp-nwk-atm-xr.gpi.remote.binc.net')
 
 
 if __name__ == '__main__':
