3a29baca749605667b776285e5f9372f2f6836c7
Miguel Bonilla <bonilla7710@gmail.com>
2019-05-10 18:10:46 -0400

    Added a Logging Method
    * Added QueryLog class that uses lcat to pull any interface
      flap information

--- bgpInfo.py
+++ bgpInfo.py
@@ -8,6 +8,7 @@ import datetime
 import sys
 import os
 import logging
+import pprint
 
 
 class VerifyUserInput(object):
@@ -112,6 +113,35 @@ class RecursiveLookup(object):
                 (nbma_end_ip, source_interface))
         return (nbma_end_ip, source_interface)
 
+class QueryLogs(object):
+    ''' This class will contain all logs related methods '''
+    def __init__(self, ci_fqdn):
+        self.ci_fqdn = ci_fqdn
+        self.pp = pprint.PrettyPrinter(indent=2)
+
+    def query_lcat(self, interface_name):
+        bgp_logger.info('query_lcat() Method')
+        ci_name_short = self.ci_fqdn.split('.')[0]
+        ci_name_pattern = re.compile(r'\s+(%s)'%self.ci_fqdn, re.IGNORECASE)
+        try:
+            lcat_process = subprocess.Popen(
+                    ['lcat','silo'], stdout=subprocess.PIPE)
+            grep1_process = subprocess.Popen(
+                    ['grep',ci_name_short], stdin=lcat_process.stdout,
+                    stdout=subprocess.PIPE)
+            grep2_process = subprocess.Popen(
+                    ['grep',interface_name], stdin=grep1_process.stdout,
+                    stdout=subprocess.PIPE)
+            stdout = grep2_process.communicate()[0]
+            stdout = stdout.split('\n')
+            bgp_logger.info('Log Information: %s'% self.pp.pformat(stdout))
+        except Exception as err:
+            bgp_logger.info(err)
+            raise SystemExit(
+                "I am not able to query silo logs\n")
+
+    def query_cisco_device_log(self):
+        pass
 
 class CiscoCommands(RecursiveLookup):
     ''' This class will run any bgp related commands '''
@@ -202,6 +232,22 @@ class CiscoCommands(RecursiveLookup):
             bgp_logger.info('did not find a VRF')
             return None
 
+    def show_intf_desciption(self, nbma_interface):
+        ''' This method will extract interface description
+            if there's a circuit ID it can be used to open
+            a carrier ticket '''
+        bgp_logger.info('show_interface_desciption() method')
+        self.command = 'show run int ' + nbma_interface + ' | i description'
+        output = self.run_cisco_commands()
+        description = None
+        int_desc_pattern = re.compile(r'(?:description\s+)(.+)')
+        for line in output[1:]:
+            if int_desc_pattern.search(line):
+                description = int_desc_pattern.search(line).group(1)
+                return description
+        if not description:
+            return None
+
     def ping_through_vrf(self, vrf_name, nbma_end_ip):
         ''' this method will ping nbma end point ip address 
             through vrf to test carrier connectivity '''
@@ -254,23 +300,36 @@ def bgp_orchestrator(ci_fqdn, neighbor_ip):
     bgp_logger.info('bgp_orchestrator() method')
     bgp = CiscoCommands(ci_fqdn)
     bgp_as = bgp.verify_ip_protocols()
+
+    query_logging = QueryLogs(ci_fqdn)
+
     if bgp_as:
         nexthop_ip, source_interface = bgp.show_ip_cef(neighbor_ip)
         bgp_logger.info("nexthop ip: %s , interface %s"%
             (nexthop_ip,source_interface))
 
+        # if cef points to a Tunnel then recursive lookup
+        # to find nmba tunnel destination IP
         if 'Tunnel' in source_interface:
             tunnel_dest_ip, source_interface = (
                 bgp.is_tunnel(source_interface))
         else:
             tunnel_dest_ip = None
 
+        # look if there's a VRF associated with interface
         if nexthop_ip:
             vrf_name = bgp.show_vrf_config(source_interface)
-
         if tunnel_dest_ip:
             vrf_name = bgp.show_vrf_config(source_interface)
 
+        # query the silo logs for interface flap
+        if nexthop_ip:
+            query_logging.query_lcat(source_interface)
+        if tunnel_dest_ip:
+            query_logging.query_lcat(source_interface)
+
+        # if vrf associated with interface, use it to ping
+        # gateway, other endpoint or end of tunnel nbma
         if vrf_name:
             if nexthop_ip:
                 ping_vrf_results = bgp.ping_through_vrf(
@@ -278,13 +337,28 @@ def bgp_orchestrator(ci_fqdn, neighbor_ip):
             if tunnel_dest_ip:
                 ping_vrf_results = bgp.ping_through_vrf(
                                     vrf_name, tunnel_dest_ip)
+        
+        # Retreive Interface Description
+        if nexthop_ip:
+            description = bgp.show_intf_desciption(source_interface)
+            if description:
+                bgp_logger.info('Interface description: %s'% description)
+            else:
+                bgp_logger.info('No interface description found!')
+        if tunnel_dest_ip:
+            description = bgp.show_intf_desciption(source_interface)
+            if description:
+                bgp_logger.info('Interface description: %s'% description)
+            else:
+                bgp_logger.info('No interface description found!'
+
         if not vrf_name:
             if nexthop_ip:
                 ping_results = bgp.ping_through_to_end_ip(
                     nexthop_ip)
             if tunnel_dest_ip:
                 ping_results = bgp.ping_through_to_end_ip(
-                    tunnel_dest_ip)      
+                    tunnel_dest_ip)    
     else:
         raise SystemExit("This device does not run BGP")
 
